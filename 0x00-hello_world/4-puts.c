3ec2703e96a4ff0c1a0b2d021c090f5200ec9761
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2022-10-13 16:17:59 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
  * main - puts"\"Programming is like building a multilingual puzzle"
  * return: 0
  * Discription: this function prints out strings
+ * char JOSHUA - this is a function to accept strings
  */
-void  main(void)
+int  main(void)
 {
-	char JOSHUA[] = "\"Programming is like building a multilingual puzzle"
-
+	char JOSHUA[] = "\"Programming is like building a multilingual puzzle\n";
 	puts(JOSHUA);
 	return (0);
 }
