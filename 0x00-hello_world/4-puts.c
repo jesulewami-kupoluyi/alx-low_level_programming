802ab4cc5d1463fe96c98271ec99f5f9092b3329
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2022-10-13 15:26:15 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
+/**
+ * This is a function that prints main
+ * */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
