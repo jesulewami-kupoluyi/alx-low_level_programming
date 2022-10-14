d984b220998e909f13c1b5649deec585e0f45c8e
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2022-10-13 18:25:39 -0700

    update

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - This function prints out the list of size
+ * Return: 0 if the result is valid if not print non-zero value
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+	return (0);
+}
