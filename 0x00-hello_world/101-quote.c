ffca05492f4bd361b41590d275f3f6508232afa1
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2022-10-13 20:07:14 -0700

    update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - this function prints an output string
+ * Return: 1 if the output is valid if not echo 2
+ */
+int main(void)
+{
+	write(KUPS, "and that piece of art is useful\"
+		- Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
