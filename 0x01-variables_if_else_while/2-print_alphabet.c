03294f4a79daafe4fadbbdb2133a6b64b7d76628
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2022-10-15 07:32:26 -0700

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main -Entry function
+ * Return:0 if true
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+	putchar (n);
+	putchar ('\n');
+	}
+	return (0);
+
+}
