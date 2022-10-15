e0c2efb4ec157a534420433a1928ddca8ea6c6a0
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2022-10-15 12:18:27 -0700

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main -Entry function
+ * Return:0 if true
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
