7d296891226ecf2fde1b167f5d41c6fd9bcf331d
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2022-10-15 13:41:25 -0700

    update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	char n;
+	int n;
 
-	for (n = 'a'; n <= 'z'; n++)
-		for (n != 'e'; n != 'q'; n++)
-		{
-			putchar(n);
-		}
-	putchar ('\n');
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
 	return (0);
-
 }
