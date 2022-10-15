afd82518c9519429f922b35d39e6b459a928c0f0
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2022-10-14 18:02:21 -0700

    update

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include <time.h>
+/**
+ * main-This is a program that prints out the var n
+ * Return:0 if output true
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	{
+	if (n > 5)
+		printf("%dand is greater than 5\n", n);
+	else if (n == 0)
+		printf("%dand is 0\n", n);
+	else if (n < 6 && !0)
+		printf("%dand is less than 6 and not 0\n", n);
+	}
+	return (0);
+
+}
