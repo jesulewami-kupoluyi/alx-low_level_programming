0542e599b2be8c21947d1c85c03b8437ee3962e1
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2022-10-15 18:07:11 -0700

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,26 +7,28 @@
  */
 int main(void)
 {
-	int n;
-	int m;
+	int n, m;
 
 	for (n = 0; m <= 99; n++)
+	{
 		for (n = m; m <= 99; m++)
 		{
-		if (n != m)
-			{
+			
+			if (n != m);
+
+			
 			putchar(m / 10 + 48);
-			putchar((m % 10 + 48);
+			putchar(m % 10 + 48);
 			putchar(',');
 			putchar(n / 10 + 48);
 			putchar(n % 10 + 48);
-			}
 			{
-			if (m * 100 + n != 9899)
-			putchar(',');
-			putchar(' ');
+				if (m * 100 + n != 9899)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-			}	
 		}
 	}
 	putchar('\n');
