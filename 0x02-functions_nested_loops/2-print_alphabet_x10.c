c517916ac95642460b4e19912f22a6f69fa38682
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2022-10-18 16:01:39 -0700

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include<unistd.h>
 #include"main.h"
 /**
- * print_alphabet_x10
+ * print_alphabet_x10 -Entry program
  * Return: void.
  */
 void print_alphabet_x10(void)
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 
 	for (n = 'a'; n <= 'z'; n++)
 	{
-		_putchar(n * 10);
+		_putchar(n);
 
 	}
 	_putchar('\n');
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include<unistd.h>
+#include"main.h"
+/**
+ * _islower(int c) -Entry program.
+ * Return: 1 if true otherwise 0
+ */
+int _islower(int c)
+{
+	int c;
+
+	if (c = 'a';  <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
