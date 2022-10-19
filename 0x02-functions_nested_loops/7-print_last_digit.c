0d39682e43c1a70ad41852f32fad9f12a41bb771
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2022-10-18 21:23:38 -0700

    update

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * print_last_digit -Entry program
+ * @n: integar
+ * Return:0 if true
+ */
+int print_last_digit(int n)
+{
+	int res = n % 10;
+
+	if (n < o)
+	{
+		res = res * -1;
+	}
+	_putchar(res + '0');
+	return (res);
+}
