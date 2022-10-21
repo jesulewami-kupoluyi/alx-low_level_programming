6b5f1d1c58441f1716c36f03db8d4f3fc133eac8
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2022-10-20 19:34:21 -0700

    update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,17 +10,21 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest = b;
+	int largest;
 
-	if (a > b)
+	if (a >= b && b >= c)
 	{
 		largest a;
 	}
-	else if (c > b)
+	else if (b >= a && a >= c)
+
+	{
+		largest b;
+	}
+	else
 	{
 		largest c;
 	}
-
 	return (largest);
 
 }
