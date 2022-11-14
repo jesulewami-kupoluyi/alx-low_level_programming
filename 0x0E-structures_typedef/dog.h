ca9037f09ee72086e9a2e2224200f9caefadae63
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2022-11-14 15:31:08 -0800

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog -structure of a set of  values.
+ * @name: character value
+ * @age: float value
+ * @owner: character value
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
