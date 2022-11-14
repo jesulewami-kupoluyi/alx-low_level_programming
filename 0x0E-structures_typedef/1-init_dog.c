bf7612c54ab0415bef28106469b589620ad3022a
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2022-11-14 15:44:55 -0800

    update

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"main.h"
+#include"dog.h"
+/**
+ * main -initializes a function call.
+ * Return: 0 if true.
+ */
+int main(void)
+{
+	struct dog my_dog;
+
+	init_dog(&my_dog, "Poppy", 3.5, "Bob");
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	return (0);
+}
