c7d4edf5e0a4833cef3f7fed07c4c2e3d30305bf
jesulewami-kupoluyi <jesulewamikupoluyi@gmail.com>
2022-11-16 16:08:35 -0800

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
-#include"function_pointers.h"
+#include "function_pointers.h"
+
 /**
- * print_name -function that prints name.
- * @name: value
- * @f: pointer to function
- * Return: nothing
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
  */
 void print_name(char *name, void (*f)(char *))
 {
@@ -12,4 +12,3 @@ void print_name(char *name, void (*f)(char *))
 
 	f(name);
 }
-
